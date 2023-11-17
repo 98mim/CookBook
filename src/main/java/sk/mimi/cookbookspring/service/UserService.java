d619ce547b1593cb708ed5b3d3774a2b61ce0f2f
michaelapolakova <mimipolakova@gmail.com>
2023-11-18 00:40:22 +0100

    add user option for changing password

---
+++ src/main/java/sk/mimi/cookbookspring/service/UserService.java
@@ -0,0 +1,34 @@
+package sk.mimi.cookbookspring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import sk.mimi.cookbookspring.DTO.model.ChangePasswordRequest;
+import sk.mimi.cookbookspring.model.UserEntity;
+import sk.mimi.cookbookspring.repository.UserRepository;
+
+import java.security.Principal;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
+    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
+
+        var user = (UserEntity) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalStateException("Wrong password");
+        }
+        if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
+            throw new IllegalStateException("Password are not the same");
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+
+        repository.save(user);
+    }
+}
